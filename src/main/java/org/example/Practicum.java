4e5547f14b57fcc3e8d8e1f59f4c3369e4ef8e4c
Michael <mikhail6886@yandex.ru>
2024-09-02 11:35:22 +0500

    update the class

--- src/main/java/org/example/Practicum.java
+++ src/main/java/org/example/Practicum.java
@@ -26,4 +26,30 @@ public class Practicum {
         homka.eat(15);
         System.out.println("Новый вес: " + homka.weight);
     }
+    public static class Hamster {
+
+        // Поля
+        String name = "Байт"; // Имя
+        int age = 2; // Возраст
+        String color = "Рыжий"; // Цвет
+        int weight = 350; // Вес
+
+        // Методы
+        // Есть
+        void eat(int foodWeight) {
+            weight = weight + foodWeight;
+        }
+
+        // Бегать в колесе
+        void runInWheel() {
+            System.out.println("Бегу-бегу-бегу!");
+            weight = weight - 5;
+        }
+
+        // Прятать семечки
+        void hideSeeds(int seedWeight) {
+            weight = weight + seedWeight;
+            System.out.println("Зимой не заголодаю.");
+        }
+    }
 }
